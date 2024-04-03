e7d0730c85b22462fdf3647d78044e815bb9a858
codershub-ing <90494823+CodersHub-in@users.noreply.github.com>
2024-04-03 23:16:42 +0530

    s

---
+++ react_01/Vite_React/src/Api_Handling.jsx
@@ -0,0 +1,4 @@
+function Api_Handling() {
+    return "hi here"
+}
+export default Api_Handling;
--- react_01/Vite_React/src/main.jsx
+++ react_01/Vite_React/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Api_Handling from './Api_Handling.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot (document.getElementById ('root')).render (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Api_Handling/>
+  </React.StrictMode>
+);
