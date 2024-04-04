56c5d30baee909f8d60b9692c43a4b096fc5a3bf
codershub-ing <90494823+CodersHub-in@users.noreply.github.com>
2024-04-05 02:03:45 +0530

    some files name changes and PassWord Generator project completed

--- 04_BgChanger/src/App.jsx
+++ 04_BgChanger/src/App.jsx
@@ -3,13 +3,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [bgColor, setBgColor] = useState("black")
   return (
-    <>
-      <p> {count}</p>
-      <button onClick={() => { setCount(count + 1) }} className='text-3xl border-solid border-2 border-sky-500 rounded-xl p-2 font-bold p12 bg-red '>click</button>
-    </>
+    <div className='h-screen w-full' style={{backgroundColor:bgColor}}> 
+      <div className='w-3/4 bg-white flex flex-wrap justify-center fixed mx-auto bottom-10  left-0 right-0 rounded-full'>
+        <button onClick={()=>setBgColor("red")} className=' p-2 m-1 bg-red-500 rounded-full w-32 ' >Red</button>
+        <button onClick={()=>setBgColor("black")} className=' p-2 m-1 bg-black rounded-full w-32 ' >Black</button>
+        <button onClick={()=>setBgColor("violet")} className=' p-2 m-1 bg-violet-500 rounded-full w-32 ' >Violet</button>
+        <button onClick={()=>setBgColor("pink")} className=' p-2 m-1 bg-pink-500 rounded-full w-32 ' >Pink</button>
+        <button onClick={()=>setBgColor("gray")} className=' p-2 m-1 bg-gray-500 rounded-full w-32 ' >Gray</button>
+      </div>
+
+    </div>
   )
 }
 
--- 05_PassWord_Ganarater/src/main.jsx
+++ 05_PassWord_Ganarater/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Nav from'./Nav_Bar/Nav.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav/>
-    <App /> 
-    
+    <App />
   </React.StrictMode>,
 )
